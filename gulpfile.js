0facf3d856070657f0b4af2cce94fefe27aec722
Neha Gupta <gnehapk@gmail.com>
2018-09-12 13:51:08 +0900

    Added branding changes Bugzilla: 1627988 tendrl-github-id: https://github.com/Tendrl/ui/issues/1067

--- gulpfile.js
+++ gulpfile.js
@@ -110,7 +110,8 @@ gulp.task("jsLibraries", function() {
         "node_modules/angular-patternfly/node_modules/angular-drag-and-drop-lists/angular-drag-and-drop-lists.js",
         "node_modules/datatables/media/js/jquery.dataTables.js",
         "node_modules/angular-patternfly/node_modules/angularjs-datatables/dist/angular-datatables.js",
-        "node_modules/bootstrap-select/dist/js/bootstrap-select.js"
+        "node_modules/bootstrap-select/dist/js/bootstrap-select.js",
+        "node_modules/angular-detect-browser/dist/angular-detect-browser.js"
     ])
     .pipe(uglify())
     .pipe(concat("libraries.js"))
